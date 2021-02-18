06e0be697424a1c0e9861e9307e82d5cc516eea4
Samuel <60698168+SamuelGalaxys@users.noreply.github.com>
2021-02-18 15:40:13 +0900

    Add files via upload

--- data/js/dashboard.js
+++ data/js/dashboard.js
@@ -1,247 +1,289 @@
-(function($) {
-  'use strict';
-  $(function() {
-
-    if ($('#cash-deposits-chart').length) {
-      var cashDepositsCanvas = $("#cash-deposits-chart").get(0).getContext("2d");
-      var data = {
-        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8"],
-        datasets: [
-          {
-            label: 'Returns',
-            data: [27, 35, 30, 40, 52, 48, 54, 46, 70],
-            borderColor: [
-              '#ff4747'
-            ],
-            borderWidth: 2,
-            fill: false,
-            pointBackgroundColor: "#fff"
-          },
-          {
-            label: 'Sales',
-            data: [29, 40, 37, 48, 64, 58, 70, 57, 80],
-            borderColor: [
-              '#4d83ff'
-            ],
-            borderWidth: 2,
-            fill: false,
-            pointBackgroundColor: "#fff"
-          },
-          {
-            label: 'Loss',
-            data: [90, 62, 80, 63, 72, 62, 40, 50, 38],
-            borderColor: [
-              '#ffc100'
-            ],
-            borderWidth: 2,
-            fill: false,
-            pointBackgroundColor: "#fff"
-          }
-        ]
-      };
-      var options = {
+$(function () {
+    "use strict";
+    var StatsLineOptions = {
         scales: {
-          yAxes: [{
-            display: true,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-              zeroLineColor: "#e9e9e9",
-            },
-            ticks: {
-              min: 0,
-              max: 100,
-              stepSize: 20,
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }],
-          xAxes: [{
-            display: true,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-            },
-            ticks : {
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }]
+            responsive: false,
+            maintainAspectRatio: true,
+            yAxes: [{
+                display: false
+            }],
+            xAxes: [{
+                display: false
+            }]
         },
         legend: {
-          display: false
-        },
-        legendCallback: function(chart) {
-          var text = [];
-          text.push('<ul class="dashboard-chart-legend">');
-          for(var i=0; i < chart.data.datasets.length; i++) {
-            text.push('<li><span style="background-color: ' + chart.data.datasets[i].borderColor[0] + ' "></span>');
-            if (chart.data.datasets[i].label) {
-              text.push(chart.data.datasets[i].label);
-            }
-          }
-          text.push('</ul>');
-          return text.join("");
+            display: false
         },
         elements: {
-          point: {
-            radius: 3
-          },
-          line :{
-            tension: 0
-          }
+            point: {
+                radius: 0
+            }
         },
-        stepsize: 1,
-        layout : {
-          padding : {
-            top: 0,
-            bottom : -10,
-            left : -10,
-            right: 0
-          }
-        }
-      };
-      var cashDeposits = new Chart(cashDepositsCanvas, {
-        type: 'line',
-        data: data,
-        options: options
-      });
-      document.getElementById('cash-deposits-chart-legend').innerHTML = cashDeposits.generateLegend();
+        stepsize: 100
     }
-
-    if ($('#total-sales-chart').length) {
-      var totalSalesChartCanvas = $("#total-sales-chart").get(0).getContext("2d");
-
-      var data = {
-        labels: ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9",'10', '11','12', '13', '14', '15','16', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26','27','28','29', '30','31', '32', '33', '34', '35', '36', '37','38', '39', '40'],
-        datasets: [
-          {
-            label: '2019',
-            data: [42, 42, 30, 30, 18, 22, 16, 21, 22, 22, 22, 20, 24, 20, 18, 22, 30, 34 ,32, 33, 33, 24, 32, 34 , 30, 34, 19 ,34, 18, 10, 22, 24, 20, 22, 20, 21, 10, 10, 5, 9, 14 ],
-            borderColor: [
-              'transparent'
-            ],
-            borderWidth: 2,
-            fill: true,
-            backgroundColor: "rgba(47,91,191,0.77)"
-          },
-          {
-            label: '2018',
-            data: [35, 28, 32, 42, 44, 46, 42, 50, 48, 30, 35, 48, 42, 40, 54, 58, 56, 55, 59, 58, 57, 60, 66, 54, 38, 40, 42, 44, 42, 43, 42, 38, 43, 41, 43, 50, 58 ,58, 68, 72, 72 ],
-            borderColor: [
-              'transparent'
-            ],
-            borderWidth: 2,
-            fill: true,
-            backgroundColor: "rgba(77,131,255,0.77)"
-          },
-          {
-            label: 'Past years',
-            data: [98, 88, 92, 90, 98, 98, 90, 92, 78, 88, 84, 76, 80, 72, 74, 74, 88, 80, 72, 62, 62, 72, 72, 78, 78, 72, 75, 78, 68, 68, 60, 68, 70, 75, 70, 80, 82, 78, 78, 84, 82 ],
-            borderColor: [
-              'transparent'
-            ],
-            borderWidth: 2,
-            fill: true,
-            backgroundColor: "rgba(77,131,255,0.43)"
-          }
-        ]
-      };
-      var options = {
-        scales: {
-          yAxes: [{
-            display: false,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-              zeroLineColor: "#e9e9e9",
+    if ($('#stat-line_1').length) {
+        var lineChartCanvas = $("#stat-line_1").get(0).getContext("2d");
+        var gradientStroke = lineChartCanvas.createLinearGradient(100, 60, 30, 0);
+        gradientStroke.addColorStop(0, '#B721FF');
+        gradientStroke.addColorStop(1, '#21D4FD');
+        var lineChart = new Chart(lineChartCanvas, {
+            type: 'line',
+            data: {
+                labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+                datasets: [{
+                    label: 'Profit',
+                    data: [7, 6, 9, 7, 8, 6, 8, 5, 7, 8, 6, 7, 7],
+                    borderColor: gradientStroke,
+                    borderWidth: 3,
+                    fill: false
+                }]
             },
-            ticks: {
-              display : true,
-              min: 0,
-              max: 100,
-              stepSize: 10,
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }],
-          xAxes: [{
-            display: false,
-            gridLines: {
-              drawBorder: false,
-              lineWidth: 1,
-              color: "#e9e9e9",
-            },
-            ticks : {
-              display: true,
-              fontColor: "#6c7383",
-              fontSize: 16,
-              fontStyle: 300,
-              padding: 15
-            }
-          }]
-        },
-        legend: {
-          display: false
-        },
-        legendCallback: function(chart) {
-          var text = [];
-          text.push('<ul class="dashboard-chart-legend mb-0 mt-4">');
-          for(var i=0; i < chart.data.datasets.length; i++) {
-            text.push('<li><span style="background-color: ' + chart.data.datasets[i].backgroundColor + ' "></span>');
-            if (chart.data.datasets[i].label) {
-              text.push(chart.data.datasets[i].label);
-            }
-          }
-          text.push('</ul>');
-          return text.join("");
-        },
-        elements: {
-          point: {
-            radius: 0
-          },
-          line :{
-            tension: 0
-          }
-        },
-        stepsize: 1,
-        layout : {
-          padding : {
-            top: 0,
-            bottom : 0,
-            left : 0,
-            right: 0
-          }
-        }
-      };
-      var totalSalesChart = new Chart(totalSalesChartCanvas, {
-        type: 'line',
-        data: data,
-        options: options
-      });
-      document.getElementById('total-sales-chart-legend').innerHTML = totalSalesChart.generateLegend();
+            options: StatsLineOptions
+        });
     }
-
-    $('#recent-purchases-listing').DataTable({
-      "aLengthMenu": [
-        [5, 10, 15, -1],
-        [5, 10, 15, "All"]
-      ],
-      "iDisplayLength": 10,
-      "language": {
-        search: ""
-      },
-      searching: false, paging: false, info: false
-    });
-
-  });
-})(jQuery);
+    if ($('#stat-line_2').length) {
+        var lineChartCanvas = $("#stat-line_2").get(0).getContext("2d");
+        var gradientStroke = lineChartCanvas.createLinearGradient(100, 60, 30, 0);
+        gradientStroke.addColorStop(0, '#08AEEA');
+        gradientStroke.addColorStop(1, '#2AF598');
+        var lineChart = new Chart(lineChartCanvas, {
+            type: 'line',
+            data: {
+                labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+                datasets: [{
+                    label: 'Profit',
+                    data: [7, 6, 9, 7, 8, 6, 8, 5, 7, 8, 6, 7, 7],
+                    borderColor: gradientStroke,
+                    borderWidth: 3,
+                    fill: false
+                }]
+            },
+            options: StatsLineOptions
+        });
+    }
+    if ($('#stat-line_3').length) {
+        var lineChartCanvas = $("#stat-line_3").get(0).getContext("2d");
+        var gradientStroke = lineChartCanvas.createLinearGradient(100, 60, 30, 0);
+        gradientStroke.addColorStop(0, '#FEE140');
+        gradientStroke.addColorStop(1, '#FA709A');
+        var lineChart = new Chart(lineChartCanvas, {
+            type: 'line',
+            data: {
+                labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+                datasets: [{
+                    label: 'Profit',
+                    data: [7, 6, 9, 7, 8, 6, 8, 5, 7, 8, 6, 7, 7],
+                    borderColor: '#6d7cfc',
+                    borderColor: gradientStroke,
+                    borderWidth: 3,
+                    fill: false
+                }]
+            },
+            options: StatsLineOptions
+        });
+    }
+    if ($('#stat-line_4').length) {
+        var lineChartCanvas = $("#stat-line_4").get(0).getContext("2d");
+        var gradientStroke = lineChartCanvas.createLinearGradient(100, 60, 30, 0);
+        gradientStroke.addColorStop(0, '#ff7fc7');
+        gradientStroke.addColorStop(1, '#43b4ff');
+        var lineChart = new Chart(lineChartCanvas, {
+            type: 'line',
+            data: {
+                labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13"],
+                datasets: [{
+                    label: 'Profit',
+                    data: [7, 6, 9, 7, 8, 6, 8, 5, 7, 8, 6, 7, 7],
+                    borderColor: '#6d7cfc',
+                    borderColor: gradientStroke,
+                    borderWidth: 3,
+                    fill: false
+                }]
+            },
+            options: StatsLineOptions
+        });
+    }
+    if ($("#followers-bar-chart").length) {
+        var a = {
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                scales: {
+                    responsive: !0,
+                    maintainAspectRatio: !0,
+                    yAxes: [{
+                        display: !0,
+                        gridLines: {
+                            color: "rgba(0, 0, 0, 0.03)",
+                            drawBorder: !1
+                        },
+                        ticks: {
+                            min: 20,
+                            max: 80,
+                            stepSize: 20,
+                            fontColor: "#212529",
+                            maxTicksLimit: 3,
+                            callback: function (a, e, r) {
+                                return a + " K"
+                            },
+                            padding: 10
+                        }
+                    }],
+                    xAxes: [{
+                        display: !1,
+                        barPercentage: .3,
+                        gridLines: {
+                            display: !1,
+                            drawBorder: !1
+                        }
+                    }]
+                },
+                legend: {
+                    display: !1
+                }
+            },
+            e = document.getElementById("followers-bar-chart");
+        new Chart(e, {
+            type: "bar",
+            data: {
+                labels: ["Mon", "Tue", "Wed", "Thus", "Fri", "Sat"],
+                datasets: [{
+                    label: "Follower",
+                    data: [62, 52, 73, 58, 63, 72],
+                    backgroundColor: [chartColors[0], chartColors[0], chartColors[0], chartColors[0], chartColors[0], chartColors[0]],
+                    borderColor: chartColors[0],
+                    borderWidth: 0
+                }]
+            },
+            options: a
+        })
+    }
+    if ($("#radial-chart").length) {
+        a = {
+            chart: {
+                height: 230,
+                type: "radialBar"
+            },
+            series: [67],
+            colors: ["#696ffb"],
+            plotOptions: {
+                radialBar: {
+                    hollow: {
+                        margin: 0,
+                        size: "70%",
+                        background: "rgba(255,255,255,0.1)"
+                    },
+                    track: {
+                        dropShadow: {
+                            enabled: !0,
+                            top: 2,
+                            left: 0,
+                            blur: 4,
+                            opacity: .02
+                        }
+                    },
+                    dataLabels: {
+                        name: {
+                            offsetY: -10,
+                            color: "#adb5bd",
+                            fontSize: "13px"
+                        },
+                        value: {
+                            offsetY: 5,
+                            color: "#000",
+                            fontSize: "20px",
+                            show: !0
+                        }
+                    }
+                }
+            },
+            fill: {
+                type: "gradient",
+                gradient: {
+                    shade: "dark",
+                    type: "vertical",
+                    gradientToColors: ["#87D4F9"],
+                    stops: [0, 100]
+                }
+            },
+            stroke: {
+                lineCap: "round"
+            },
+            labels: ["Progress"]
+        };
+        (r = new ApexCharts(document.querySelector("#radial-chart"), a)).render()
+    }
+    if ($("#cpu-performance").length) {
+        var r, y = (r = document.getElementById("cpu-performance").getContext("2d")).createLinearGradient(0, 0, 0, 0);
+        y.addColorStop(0, "rgba(255, 255, 225,0.5)"), y.addColorStop(1, "rgba(255, 255, 225,0.5)");
+        new Chart(r, {
+            type: "line",
+            data: {
+                labels: ["Day 1", "Day 2", "Day 3", "Day 4", "Day 5", "Day 6", "Day 7", "Day 8", "Day 9", "Day 10", "Day 11", "Day 12", "Day 13", "Day 14", "Day 15", "Day 16", "Day 17", "Day 18", "Day 19", "Day 20", "Day 21", "Day 22", "Day 23", "Day 24", "Day 25", "Day 26", "Day 27", "Day 28", "Day 29", "Day 30", "Day 31", "Day 32", "Day 33", "Day 34", "Day 35", "Day 36", "Day 37", "Day 38", "Day 39", "Day 40", "Day 41", "Day 42", "Day 43", "Day 44", "Day 45", "Day 46", "Day 47", "Day 48", "Day 49", "Day 50", "Day 51", "Day 52", "Day 53", "Day 54", "Day 55", "Day 56", "Day 57", "Day 58", "Day 59", "Day 60", "Day 61", "Day 62", "Day 63", "Day 64", "Day 65", "Day 66", "Day 67", "Day 68", "Day 69", "Day 70", "Day 71", "Day 72", "Day 73", "Day 74", "Day 75", "Day 76", "Day 77", "Day 78", "Day 79", "Day 80", "Day 81", "Day 82"],
+                datasets: [{
+                    label: "CPU Usage",
+                    data: [56, 55, 59, 59, 59, 57, 56, 57, 54, 56, 58, 57, 59, 58, 59, 57, 55, 56, 54, 52, 49, 48, 50, 50, 46, 45, 49, 50, 52, 53, 52, 55, 54, 53, 56, 55, 56, 55, 54, 55, 57, 58, 56, 55, 56, 57, 58, 59, 58, 57, 55, 53, 52, 55, 57, 55, 54, 52, 55, 57, 56, 57, 58, 59, 58, 59, 57, 56, 55, 57, 58, 59, 60, 62, 60, 59, 58, 57, 56, 57, 56, 58, 59],
+                    borderColor: chartColors[0],
+                    backgroundColor: y,
+                    borderWidth: 3
+                }, {
+                    label: "Ram Usage",
+                    data: [32, 25, 29, 29, 29, 27, 26, 27, 24, 26, 28, 27, 29, 28, 29, 27, 25, 26, 24, 20, 18, 21, 19, 17, 14, 13, 16, 15, 17, 18, 19, 22, 20, 23, 21, 25, 24, 22, 25, 27, 25, 26, 24, 20, 18, 18, 18, 22, 19, 23, 25, 23, 22, 25, 27, 25, 24, 22, 25, 27, 26, 27, 28, 29, 28, 29, 27, 26, 25, 27, 28, 29, 26, 27, 25, 29, 28, 27, 26, 27, 26, 28, 29],
+                    borderColor: chartColors[1],
+                    backgroundColor: "rgba(255,255,255,0.05)",
+                    borderWidth: 3
+                }]
+            },
+            options: {
+                responsive: !0,
+                animation: {
+                    animateScale: !0,
+                    animateRotate: !0
+                },
+                elements: {
+                    point: {
+                        radius: 0
+                    }
+                },
+                layout: {
+                    padding: {
+                        left: 0,
+                        right: 0,
+                        top: 0,
+                        bottom: 0
+                    }
+                },
+                legend: !1,
+                stepsize: 150,
+                min: 0,
+                max: 350,
+                scales: {
+                    xAxes: [{
+                        gridLines: {
+                            display: !1,
+                            drawBorder: !1
+                        },
+                        ticks: {
+                            display: !1,
+                            max: 150
+                        }
+                    }],
+                    yAxes: [{
+                        gridLines: {
+                            display: !1,
+                            drawBorder: !1
+                        },
+                        ticks: {
+                            display: !1
+                        }
+                    }]
+                }
+            }
+        })
+    }
+});
--- data/js/template.js
+++ data/js/template.js
@@ -1,60 +1,93 @@
-(function($) {
-  'use strict';
-  $(function() {
-    var body = $('body');
-    var contentWrapper = $('.content-wrapper');
-    var scroller = $('.container-scroller');
-    var footer = $('.footer');
-    var sidebar = $('.sidebar');
+'use strict';
 
-    //Add active class to nav-link based on url dynamically
-    //Active class can be hard coded directly in html file also as required
 
-    function addActiveClass(element) {
-      if (current === "") {
-        //for root url
-        if (element.attr('href').indexOf("index.html") !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
-        }
-      } else {
-        //for other url
-        if (element.attr('href').indexOf(current) !== -1) {
-          element.parents('.nav-item').last().addClass('active');
-          if (element.parents('.sub-menu').length) {
-            element.closest('.collapse').addClass('show');
-            element.addClass('active');
-          }
-          if (element.parents('.submenu-item').length) {
-            element.addClass('active');
-          }
+// THEME COLORS
+var style = getComputedStyle(document.body);
+var chartColors = ["#696ffb", "#7db8f9", "#05478f", "#00cccc", "#6CA5E0", "#1A76CA"];
+var primaryColor = style.getPropertyValue('--primary');
+var secondaryColor = style.getPropertyValue('--secondary');
+var successColor = style.getPropertyValue('--success');
+var warningColor = style.getPropertyValue('--warning');
+var dangerColor = style.getPropertyValue('--danger');
+var infoColor = style.getPropertyValue('--info');
+var darkColor = style.getPropertyValue('--dark');
+
+
+// BODY ELEMENTS
+var Body = $("body");
+var TemplateSidebar = $('.sidebar');
+var TemplateHeader = $('.t-header');
+var PageContentWrapper = $(".page-content-wrapper");
+var DesktopToggler = $(".t-header-desk-toggler");
+var MobileToggler = $(".t-header-mobile-toggler");
+
+// SIDEBAR TOGGLE FUNCTION FOR MOBILE (SCREEN "MD" AND DOWN)
+MobileToggler.on("click", function () {
+  $(".page-body").toggleClass("sidebar-collpased");
+});
+
+
+// CHECK FOR CURRENT PAGE AND ADDS AN ACTIVE CLASS FOR TO THE ACTIVE LINK
+var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
+$('.navigation-menu li a', TemplateSidebar).each(function () {
+  var $this = $(this);
+  if (current === "") {
+    //FOR ROOT URL
+    if ($this.attr('href').indexOf("index.html") !== -1) {
+      $(this).parents('li').last().addClass('active');
+      if ($(this).parents('.navigation-submenu').length) {
+        $(this).addClass('active');
+      }
+    }
+  } else {
+    //FOR OTHER URL
+    if ($this.attr('href').indexOf(current) !== -1) {
+      $(this).parents('li').last().addClass('active');
+      if ($(this).parents('.navigation-submenu').length) {
+        $(this).addClass('active');
+      }
+      if (current !== "index.html") {
+        $(this).parents('li').last().find("a").attr("aria-expanded", "true");
+        if ($(this).parents('.navigation-submenu').length) {
+          $(this).closest('.collapse').addClass('show');
         }
       }
     }
+  }
+});
 
-    var current = location.pathname.split("/").slice(-1)[0].replace(/^\/|\/$/g, '');
-    $('.nav li a', sidebar).each(function() {
-      var $this = $(this);
-      addActiveClass($this);
-    })
+$(".btn.btn-refresh").on("click", function () {
+  $(this).addClass("clicked");
+  setTimeout(function () {
+    $(".btn.btn-refresh").removeClass("clicked");
+  }, 3000);
+});
 
-    //Close other submenu in sidebar on opening any
 
-    sidebar.on('show.bs.collapse', '.collapse', function() {
-      sidebar.find('.collapse.show').collapse('hide');
+$(".btn.btn-like").on("click", function () {
+  $(this).toggleClass("clicked");
+  $(this).find("i").toggleClass("mdi-heart-outline clicked").toggleClass("mdi-heart");
+});
+
+function purchaseBanner() {
+  var bannerState = localStorage.getItem('bannerState') ? localStorage.getItem('bannerState') : "enabled";
+  if (bannerState == "enabled") {
+    $("body").addClass("purchase-banner-active");
+    $("body").prepend('\
+          <div class= "item-purchase-banner">\
+            <p class="font-weight-medium banner-text">Upgrade to Premium For More Pro Features</p>\
+              <a href = "http://www.uxcandy.co/product/label-pro-admin-template/" target = "_blank" class = "banner-button btn btn-primary btn-icon" > \
+                <i class="mdi mdi-cart mr-2"></i>Buy Now\
+              </a>\
+              <span class="toggler-close"><i class="mdi mdi-close"></i></span>\
+          </div>\
+        ')
+    $(".item-purchase-banner .toggler-close").on("click", function () {
+      $(".item-purchase-banner").slideUp(300);
+      $("body").removeClass("purchase-banner-active");
+      localStorage.setItem('bannerState', "disabled");
     });
+  }
+}
 
-
-    //Change sidebar
-
-    $('[data-toggle="minimize"]').on("click", function() {
-      body.toggleClass('sidebar-icon-only');
-    });
-
-    //checkbox and radios
-    $(".form-check label,.form-radio label").append('<i class="input-helper"></i>');
-  });
-})(jQuery);
+purchaseBanner();
