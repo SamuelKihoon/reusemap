04da1315165e8e4601d529e5efd1ecac2626b336
Samuel <60698168+SamuelGalaxys@users.noreply.github.com>
2021-02-23 08:43:09 +0900

    Update scan.js

--- data/js/scan/scan.js
+++ data/js/scan/scan.js
@@ -2,8 +2,8 @@ require('babel-polyfill');
 require('webrtc-adapter');
 
 var Instascan = {
-  Scanner: require('./data/js/scan/scanner'),
-  Camera: require('./data/js/scan/camera')
+  Scanner: require('./scanner'),
+  Camera: require('./camera')
 };
 
 module.exports = Instascan;
