1a595cb16a7df318150b6826851154e0f3ef2a5a
SamuelGalaxys <samuelthegalaxys@gmail.com>
2021-04-17 18:14:02 +0900

    s

--- data/js/sessionCheck.js
+++ data/js/sessionCheck.js
@@ -12,7 +12,7 @@
               //자바스크립트 dom 선택자를 통해서 네비게이션 메뉴의 엘리먼트 변경해주기
               document.getElementById("loginmenu").textContent = "로그아웃";
               document.getElementById("loginmenu").href = "login.html";
-           //   document.getElementById("joinmenu").textContent = "반가워요! " + snapshot.val().name + " 님";
+            //  document.getElementById("joinmenu").textContent = "반가워요! " + snapshot.val().name + " 님";
              // document.getElementById("joinmenu").href = "#";
               document.getElementById("leaf").textContent = snapshot.val().leaf + "개";
 //              document.getElementById("leaf").textContent = snapshot.val().leaf;
@@ -21,7 +21,8 @@
               name = snapshot.val().name;   //유저 닉네임은 계속 쓸거기 때문에 전역변수로 할당
               loginUserKey = snapshot.key;  //로그인한 유저의 key도 계속 쓸 것이기 때문에 전역변수로 할당
               userInfo = snapshot.val(); //snapshot.val()에 user 테이블에 있는 해당 개체 정보가 넘어온다. userInfo에 대입!
-              
+              leaf = snapshot.val(); //snapshot.val()에 user 테이블에 있는 해당 개체 정보가 넘어온다. userInfo에 대입!
+
                 thanksList();
 
 
